5b57bbc4f56e3802ccaba15d032f534ab2855d7a
Arthu Santiago <contato@avds.eti.br>
2022-03-01 10:11:26 -0400

    finalizado o fluxo de edição da Categoria

--- src/Controller/CategoriasController.php
+++ src/Controller/CategoriasController.php
@@ -89,25 +89,29 @@ class CategoriasController extends AppController
     /**
      * Edit method
      *
-     * @param string|null $id Categoria id.
+     * @param string $id Categoria id.
      * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function edit($id)
     {
-        $categoria = $this->Categorias->get($id, [
-            'contain' => [],
-        ]);
+        $categoria = $this->Categorias->get($id);
+        $subcategorias = $this->Categorias->Subcategorias
+        ->find()
+        ->where(['categoria_id' => $id]);
+
         if ($this->request->is(['patch', 'post', 'put'])) {
             $categoria = $this->Categorias->patchEntity($categoria, $this->request->getData());
             if ($this->Categorias->save($categoria)) {
-                $this->Flash->success(__('The categoria has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
+                $this->Flash->success(__('Salvo com sucesso'));
+            }else{
+                $this->Flash->error(null, ['params' => ['mensagens' => $categoria->getErrors()]]);
             }
-            $this->Flash->error(__('The categoria could not be saved. Please, try again.'));
+
+            return $this->redirect(['action' => 'edit', $categoria->id]);
         }
-        $this->set(compact('categoria'));
+        
+        $this->set(compact('categoria', 'subcategorias'));
     }
 
     /**
