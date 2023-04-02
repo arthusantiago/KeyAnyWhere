1c034082f89fe68c8c2e8ee0c1243659a24fc2c1
Arthu Santiago <contato@avds.eti.br>
2023-04-02 14:03:17 -0400

    criado a paginação do sistema

---
+++ config/paginator-templates.php
@@ -0,0 +1,9 @@
+<?php
+    return [
+        'nextActive'   => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
+        'nextDisabled' => '<li class="page-item disabled"><a class="page-link" href="">{{text}}</a></li>',
+        'prevActive'   => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
+        'prevDisabled' => '<li class="page-item disabled"><a class="page-link" href="">{{text}}</a></li>',
+        'counterPages' => '<li class="page-item"><a class="page-link">{{page}} de {{pages}}</a></li>',
+    ];
+?>
