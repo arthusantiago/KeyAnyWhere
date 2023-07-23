cb3f0b118ab94e6413ff31bfd01dccfcfa109773
Arthu Santiago <contato@avds.eti.br>
2023-07-23 19:39:11 -0400

    Unificado todas as migrations em uma

--- config/Migrations/20220125021729_Inicial.php
+++ config/Migrations/20220125021729_Inicial.php
@@ -20,21 +20,23 @@ class Inicial extends AbstractMigration
         ->addColumn('password', 'string', ['limit' => 255, 'null' => false])
         ->addColumn('tfa_secret', 'string', ['limit' => 144, 'null' => true, 'default' => null])
         ->addColumn('tfa_ativo', 'boolean', ['default' => false])
+        ->addColumn('root', 'boolean', ['default' => false])
         ->addColumn('created', 'datetime')
         ->addColumn('modified', 'datetime')
         ->create();
 
         $this->table('categorias')
-        ->addColumn('nome', 'string', ['limit' => 100, 'null' => false])
+        ->addColumn('nome', 'string', ['limit' => 256, 'null' => false])
+        ->addColumn('posicao', 'integer', ['null' => true, 'default' => null])
         ->addColumn('created', 'datetime')
         ->addColumn('modified', 'datetime')
         ->create();
 
         $this->table('entradas')
-        ->addColumn('titulo', 'string', ['limit' => 100, 'null' => false])
+        ->addColumn('titulo', 'string', ['limit' => 256, 'null' => false])
         ->addColumn('username', 'string', ['limit' => 256, 'null' => false])
         ->addColumn('password', 'string', ['limit' => 256, 'null' => false])
-        ->addColumn('link', 'string', ['limit' => 500, 'default' => null, 'null' => true])
+        ->addColumn('link', 'string', ['limit' => 1000, 'default' => null, 'null' => true])
         ->addColumn('anotacoes', 'text')
         ->addColumn('categoria_id', 'integer')
         ->addColumn('user_id', 'integer')
@@ -49,5 +51,31 @@ class Inicial extends AbstractMigration
         $this->table('entradas')
         ->addForeignKey('user_id', 'users', 'id', ['delete'=> 'CASCADE', 'update'=> 'CASCADE'])
         ->save();
+
+        $this->table('logs')
+        ->addColumn('evento', 'string', ['limit' => 7, 'null' => true, 'default' => null])
+        ->addColumn('nivel_severidade', 'integer', ['null' => false, 'default' => null])
+        ->addColumn('recurso', 'string', ['limit' => 100, 'null' => true, 'default' => null])
+        ->addColumn('ip_origem', 'string', ['limit' => 39, 'null' => true, 'default' => null])
+        ->addColumn('usuario', 'string', ['limit' => 200, 'null' => true, 'default' => null])
+        ->addColumn('mensagem', 'string', ['limit' => 256, 'null' => false])
+        ->addColumn('analisado', 'boolean', ['default' => false])
+        ->addColumn('created', 'datetime')
+        ->addColumn('modified', 'datetime')
+        ->addIndex(['nivel_severidade', 'analisado'])
+        ->create();
+
+        $this->table('ips_bloqueados')
+        ->addColumn('ip', 'string', ['limit' => 39, 'null' => false])
+        ->addColumn('created', 'datetime')
+        ->addColumn('modified', 'datetime')
+        ->create();
+
+        $this->table('insecure_passwords')
+        ->addColumn('password', 'string', ['limit' => 100, 'null' => false])
+        ->addColumn('created', 'datetime')
+        ->addColumn('modified', 'datetime')
+        ->addIndex(['password'])
+        ->create();
     }
 }
