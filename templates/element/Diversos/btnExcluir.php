0d2663eb6ae640d00df4508c065e15d7cf81a1af
Arthu Santiago <contato@avds.eti.br>
2023-12-14 22:07:15 -0400

    aplicando CSP no modal de exclusao de registros

--- templates/element/Diversos/btnExcluir.php
+++ templates/element/Diversos/btnExcluir.php
@@ -54,10 +54,8 @@ $idForm = "form-exclusao-{$parametros['id']}";
 <div class="modal fade" id="<?=$idModal?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
-      <div class="modal-header">
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
       <div class="modal-body">
+        <br>
         <p>Realmente deseja excluir esse registro?</p>
         <?= $this->Form->create(null, ['url' => $urlExclusao, 'id' => $idForm]); ?>
           <?= $this->Form->hidden('id', ['value' => $parametros['id']]); ?>
