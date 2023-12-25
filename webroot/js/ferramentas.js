51638c1eb0e34875d4e02264cfba1de687d3335b
Arthu Santiago <contato@avds.eti.br>
2023-12-25 19:14:25 -0400

    Corrigindo execução indevida do JS do sessionTimeout

--- webroot/js/ferramentas.js
+++ webroot/js/ferramentas.js
@@ -1,15 +1,17 @@
 /*
  * Configuração do modal e cronometro do tempo de sessão
  */
-let tempoSessao = document.getElementById('sessionTimeout').getAttribute('content') * 60;
-let myModal = new bootstrap.Modal(document.getElementById('sessaoExpiradaModal'));
-let timer = new easytimer.Timer();
-timer.start({countdown: true, startValues: {seconds: tempoSessao}});
-document.getElementById('timerSessao').innerHTML = timer.getTimeValues().toString();
-timer.addEventListener('secondsUpdated', function (e) {
+if (document.getElementById('sessionTimeout')) {
+    let tempoSessao = document.getElementById('sessionTimeout').getAttribute('content') * 60;
+    let myModal = new bootstrap.Modal(document.getElementById('sessaoExpiradaModal'));
+    let timer = new easytimer.Timer();
+    timer.start({countdown: true, startValues: {seconds: tempoSessao}});
     document.getElementById('timerSessao').innerHTML = timer.getTimeValues().toString();
-});
-timer.addEventListener('targetAchieved', () => {myModal.show()});
+    timer.addEventListener('secondsUpdated', function (e) {
+        document.getElementById('timerSessao').innerHTML = timer.getTimeValues().toString();
+    });
+    timer.addEventListener('targetAchieved', () => {myModal.show()});
+}
 
 /**
  * Exibe o conteúdo de um input=password .
