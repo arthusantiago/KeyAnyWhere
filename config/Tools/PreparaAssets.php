c20f7fd38b3923cef1a2110ca86c02c492965c98
Arthu Santiago <contato@avds.eti.br>
2024-06-08 15:12:43 -0400

    fix: Corrigindo erro na manipução dos Events do Composer

--- config/Tools/PreparaAssets.php
+++ config/Tools/PreparaAssets.php
@@ -3,7 +3,7 @@
 namespace Config\Tools;
 
 use Composer\Installer\PackageEvent;
-
+use Composer\Script\Event;
 /**
  * Trabalha os arquivos que precisam estar corretamente armazenados na pasta webroot/
  *
@@ -29,22 +29,46 @@ class PreparaAssets
     ];
 
     /**
-     * Copia os arquivos CSS e JS do pacote instalado/atualizado para devida pasta dentro do webroot/
+     * Recebe o evento de que um pacote foi atualizado
      *
      * @access	static public
      * @param	Composer\Installer\PackageEvent $event
      * @return	void
      */
-    static public function executar(PackageEvent $event)
+    public static function postPackageUpdate(PackageEvent $event)
     {
-        $installedPackage = $event->getOperation()->getPackage();
+        $nomePacote = $event->getOperation()->getTargetPackage()->getName();
+        self::executar($nomePacote);
+    }
 
-        if ($installedPackage->getName() == 'twbs/bootstrap-icons') {
+    /**
+     * Recebe o evento de que um pacote foi instalado
+     *
+     * @access	static public
+     * @param	Composer\Installer\PackageEvent $event
+     * @return	void
+     */
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $nomePacote = $event->getOperation()->getPackage()->getName();
+        self::executar($nomePacote);
+    }
+
+    /**
+     * Copia os arquivos CSS e JS do pacote instalado/atualizado para devida pasta dentro do webroot/
+     *
+     * @access static public
+     * @param string $nomePacote
+     * @return	void
+     */
+    static public function executar(string $nomePacote)
+    {
+        if ($nomePacote == 'twbs/bootstrap-icons') {
             self::excluiArquivo(self::$cssDestinoBootstrapIcons);
             self::copiarArquivo(self::$cssOrigemBootstrapIcons, self::$cssDestinoBootstrapIcons);
         }
 
-        if ($installedPackage->getName() == 'twbs/bootstrap') {
+        if ($nomePacote == 'twbs/bootstrap') {
             /* Excluir arquivos */
             self::excluirMultiplosArquivo(self::$pathDestinoCSS, self::$fileDestinoBootstrapCSS);
             self::excluirMultiplosArquivo(self::$pathDestinoJS, self::$fileDestinoBootstrapJS);
