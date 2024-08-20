a60bd9fa23885157b7808809860ade1c7c41600d
Arthu Santiago <contato@avds.eti.br>
2024-08-19 21:14:16 -0400

    feat: criado no botao de excluir as opções de escolher o tipo do botão e o texto

--- templates/element/Diversos/btnExcluir.php
+++ templates/element/Diversos/btnExcluir.php
@@ -7,16 +7,37 @@ use Cake\Core\Exception\CakeException;
   *     <?= $this->element('Diversos/modalExcluir', ['parametros' => ['controller' => 'Categoria', 'action' => 'apagar']])?>
   *
   * Exemplo de uso:
-  *     <?=$this->element('Diversos/btnExcluir', ['idRegistro' => $entrada->id])?>
-  *     É obrigatório informa o ID do registro
+  *    <?=$this->element('Diversos/btnExcluir', ['idRegistro' => $entrada->id, 'texto' => 'Finalizar', 'tipo' => 'button'])?>
+  *    'tipo':
+  *      'dropdown-item' (Default) Será exibido como uma opção em um dropdown
+  *      'button' Será exibido como um botão normal
   *
-  * @param string $idRegistro
+  * @param string $idRegistro (Obrigatório) ID do registro que será excluído
+  * @param string $texto (Opcional) Texto do botão
+  * @param string $tipo (Opcional) Tipo do botão
 **/
 
 if (!$idRegistro) {
     throw new CakeException('O ID do registro para exclusão precisa ser informado');
 }
+
+//Texto do botão
+$texto = $texto ?? 'Excluir';
+
+// Tipo do botão
+$tipo = $tipo ?? 'dropdown-item';
+$atributosHtmlBotao = '';
+
+switch ($tipo) {
+  case 'dropdown-item':
+    $atributosHtmlBotao = 'class="dropdown-item text-danger btnExcluir" ';
+    break;
+
+  case 'button':
+    $atributosHtmlBotao = 'class="btn btn-sm btn-outline-danger botoes btnExcluir" role="button"';
+    break;
+}
 ?>
-<a class="dropdown-item text-danger btnExcluir" href="#" data-excluir-id="<?=$idRegistro?>" data-excluir-modal="modalExcluirRegistro">
-  <i class="bi bi-trash2 icone-opcao"></i>Excluir
+<a <?=$atributosHtmlBotao?> href="#" data-excluir-id="<?=$idRegistro?>" data-excluir-modal="modalExcluirRegistro">
+  <i class="bi bi-trash2 icone-opcao"></i><?=$texto?>
 </a>
