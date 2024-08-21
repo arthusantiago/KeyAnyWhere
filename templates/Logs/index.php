52369ee819f58bd2e5c864ab5b980adcf5e141f0
Arthu Santiago <contato@avds.eti.br>
2024-08-21 19:53:17 -0400

    feat: aplicado novo padrão de botoes na index de logs

--- templates/Logs/index.php
+++ templates/Logs/index.php
@@ -22,8 +22,8 @@
             <tr class="text-center titulo-coluna-tabela">
                 <th><?=$this->Paginator->sort('created', 'Data')?></th>
                 <th><?=$this->Paginator->sort('nivel_severidade', 'Nível Severidade')?></th>
-                <th><?=$this->Paginator->sort('analisado', 'Status')?></th>
                 <th>Mensagem</th>
+                <th><?=$this->Paginator->sort('analisado', 'Status')?></th>
                 <th>Ações</th>
             </tr>
         </thead>
@@ -32,21 +32,23 @@
                 <tr class="text-center">
                     <td><?=$log->created?></td>
                     <td><?=$this->element('Logs/badge', ['param' => ['severidade' => $log->nivel_severidade, 'exibicao' => $log->stringNivelSeveridade()]])?></td>
-                    <td><?= $log->analisado ? 'Analisado' : 'Não analisado';?></td>
                     <td><?=h($log->mensagemEncurtada())?></td>
+                    <td><?=$log->analisado ? 'Analisado' : 'Não analisado';?></td>
                     <td>
-                        <?php if ($log->analisado): ?>
-                            <a class="btn btn-sm btn-outline-secondary botoes" role="button"  href="<?=$this->Url->build(['controller' => 'Logs', 'action' => 'analisado', $log->id])?>" title="Marcar como não analisado">
-                                <i class="bi bi-x-lg"></i>
+                        <div class="btn-group btn-group-sm botoes" role="group">
+                            <?php if ($log->analisado): ?>
+                                <a class="btn btn-sm btn-outline-secondary" role="button"  href="<?=$this->Url->build(['controller' => 'Logs', 'action' => 'analisado', $log->id])?>" title="Marcar como não analisado">
+                                    <i class="bi bi-x-lg"></i>
+                                </a>
+                            <?php else: ?>
+                                <a class="btn btn-sm btn-outline-secondary" role="button"  href="<?=$this->Url->build(['controller' => 'Logs', 'action' => 'analisado', $log->id])?>" title="Marcar como analisado">
+                                    <i class="bi bi-check-lg"></i>
+                                </a>
+                            <?php endif; ?>
+                            <a class="btn btn-sm btn-outline-secondary" role="button"  href="<?=$this->Url->build(['controller' => 'Logs', 'action' => 'view', $log->id])?>" title="Detalhes">
+                                <i class="bi bi-eye-fill"></i>Detalhes
                             </a>
-                        <?php else: ?>
-                            <a class="btn btn-sm btn-outline-secondary botoes" role="button"  href="<?=$this->Url->build(['controller' => 'Logs', 'action' => 'analisado', $log->id])?>" title="Marcar como analisado">
-                                <i class="bi bi-check-lg"></i>
-                            </a>
-                        <?php endif; ?>
-                        <a class="btn btn-sm btn-outline-secondary botoes" role="button"  href="<?=$this->Url->build(['controller' => 'Logs', 'action' => 'view', $log->id])?>" title="Detalhes">
-                            <i class="bi bi-eye-fill"></i>Detalhes
-                        </a>
+                        </div>
                     </td>
                 </tr>
             <?php endforeach; ?>
