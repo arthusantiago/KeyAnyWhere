d3a4d6c334b58ce6b59bf9ddba0dd33ead85a82e
Arthu Santiago <contato@avds.eti.br>
2024-08-28 11:18:21 -0400

    fix: melhorado o layout e arquivos do processo de configuração inicial da ferramenta

--- templates/Users/config_inicial.php
+++ templates/Users/config_inicial.php
@@ -2,30 +2,27 @@
 <h1 class="text-center">Bem-vindo KeyAnyWhere!</h1>
 <p class="text-center">Neste passo a passo vamos executar a configuração inicial da ferramenta.</p>
 <br>
-<h2 class="text-center titulo">1- Usuário de acesso</h2>
-<p class="text-center">Vamos criar o primeiro usuário do sistema, que também será o usuário master.</p>
+<h2 class="text-center titulo">Usuário de acesso</h2>
+<p class="text-center">Vamos criar o primeiro usuário do sistema, que também será o usuário <b>administrador</b>.</p>
 <?= $this->Form->create(null, ['url' => ['controller' => 'users', 'action' => 'configInicial']]); ?>
     <?php $this->Form->secure(['username', 'email', 'password']); ?>
     <div class="row justify-content-center">
         <div class="col-sm-4 mb-3">
             <label for="username" class="form-label">Nome</label>
-            <input type="text" class="form-control inputs" id="username" name="username" autocomplete="nickname" placeholder="Fulado Tal">
+            <input type="text" class="form-control inputs" id="username" name="username" autocomplete="nickname" placeholder="Fulano Silva">
         </div>
     </div>
-    <br>
     <div class="row justify-content-center">
         <div class="col-sm-4 mb-3">
             <label for="email" class="form-label">E-mail</label>
             <input type="email" class="form-control inputs" id="email" name="email" autocomplete="email" placeholder="seu@email.com.br">
         </div>
     </div>
-    <br>
     <div class="row justify-content-center">
         <div class="col-sm-4 mb-3">
             <?=$this->element('inputSenhaUser')?>
         </div>
     </div>
-    <br>
     <div class="row justify-content-center">
         <div class="col-sm-1">
             <?= $this->element('Diversos/btnSalvar')?>
--- templates/Users/config_inicial_tfa.php
+++ templates/Users/config_inicial_tfa.php
@@ -1,54 +1,22 @@
-<!doctype html>
-<html lang="pt-br">
-<head>
-    <?= $this->Html->charset() ?>
-    <?= $this->Html->meta('icon', 'favicon.ico') ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= $this->Html->meta('csrfToken', $this->request->getAttribute('csrfToken')); ?>
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-    <title>KAW - Configuração inicial</title>
-    <?= $this->Html->css([
-        'minified/bootstrap.min.css',
-        'minified/kaw.min.css',
-    ]);
-    ?>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm"></div>
-            <div class="col-sm"><?= $this->Flash->render() ?></div>
-            <div class="col-sm"></div>
+<br>
+<h1 class="text-center">Bem-vindo KeyAnyWhere!</h1>
+<p class="text-center">Neste passo a passo vamos executar a configuração inicial da ferramenta.</p>
+<br>
+<h2 class="text-center titulo">Autenticação de dois fatores (TFA)</h2>
+<div class="row justify-content-center">
+    <p class="text-center">
+        A utilização do TFA é obrigatória, por isso instale um aplicativo que execute essa função, como o <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2" class="text-decoration-none" target="_blank">Google Authenticator</a>. <br>
+        Para ativar o 2FA do usuário <strong><?=h($user->username)?> (<?=h($user->email)?>)</strong>, escaneie o QR Code com o aplicativo de TFA.
+    </p>
+</div>
+<div class="row justify-content-center"><?=$strSvgQrCode?></div>
+<?= $this->Form->create(null, ['url' => ['controller' => 'users', 'action' => 'configInicialTfa']]); ?>
+<?php $this->Form->secure(['id', 'tfa']); ?>
+    <input type="hidden" name="id" value="<?=$user->id?>"/>
+    <input type="hidden" name="tfa" value="1"/>
+    <div class="row justify-content-center">
+        <div class="col-sm-2">
+            <?= $this->element('Diversos/btnSalvar', ['parametros' => ['texto' => 'Finalizar configuração']]) ?>
         </div>
-        <br>
-        <h1 class="text-center">Bem-vindo KeyAnyWhere!</h1>
-        <p class="text-center">
-            Neste passo a passo vamos executar a configuração inicial da ferramenta.
-        </p>
-        <br>
-        <h2 class="text-center titulo">2- Autenticação de dois fatores (2FA)</h2>
-        <div class="row justify-content-center">
-            <p class="text-center">
-                A utilização do 2FA é obrigatória, por isso instale um aplicativo que execute essa função. 
-                Recomendamos o <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2" class="text-decoration-none" target="_blank">Google Authenticator</a>.
-            </p>
-            <p class="text-center">
-                Para ativar o 2FA do usuário <strong><?=h($user->username)?> (<?=h($user->email)?>)</strong>, escaneie o QR Code com o aplicativo authenticator.
-            </p>
-        </div>
-        <div class="row justify-content-center">
-            <?= $strSvgQrCode ?>
-        </div>
-        <?= $this->Form->create(null, ['url' => ['controller' => 'users', 'action' => 'configInicialTfa']]); ?>
-        <?php $this->Form->secure(['id', 'tfa']); ?>
-            <input type="hidden" name="id" value="<?=$user->id?>"/>
-            <input type="hidden" name="tfa" value="1"/>
-            <div class="row justify-content-center">
-                <div class="col-sm-2">
-                    <?= $this->element('Diversos/btnSalvar', ['parametros' => ['texto' => 'Finalizar configuração']]) ?>
-                </div>
-            </div>
-        <?= $this->Form->end(['data-type' => 'hidden']); ?>
-        <?= $this->Html->script('minified/bootstrap.min.js'); ?>
-</body>
-</html>
+    </div>
+<?= $this->Form->end(['data-type' => 'hidden']); ?>
