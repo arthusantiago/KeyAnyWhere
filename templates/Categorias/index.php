fc79bb6de0de6a64f83e731886ecb905cbfa8311
Arthu Santiago <contato@avds.eti.br>
2024-09-05 15:04:09 -0400

    feat: adicionado link na listagem da categorias.

--- templates/Categorias/index.php
+++ templates/Categorias/index.php
@@ -15,17 +15,21 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($query as $categoria): ?>
-                <tr class="text-center">
-                    <td><?=h($categoria->nomeDescrip())?></td>
-                    <td>
-                        <div class="btn-group btn-group-sm botoes" role="group">
-                            <?=$this->element('Diversos/btnEditar', ['parametros' => ['controller' => 'Categorias', 'id' => $categoria->id]])?>
-                            <?=$this->element('Diversos/btnExcluir', ['idRegistro' => $categoria->id, 'tipo' => 'button'])?>
-                        </div>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+        <?php foreach ($query as $categoria): ?>
+            <tr class="text-center">
+                <td>
+                    <a class="link-primary" href="<?=$this->Url->build(['controller' => 'Categorias', 'action' => 'listagemEntradas', $categoria->id])?>">
+                        <?=h($categoria->nomeDescrip())?>
+                    </a>
+                </td>
+                <td>
+                    <div class="btn-group btn-group-sm botoes" role="group">
+                        <?=$this->element('Diversos/btnEditar', ['parametros' => ['controller' => 'Categorias', 'id' => $categoria->id]])?>
+                        <?=$this->element('Diversos/btnExcluir', ['idRegistro' => $categoria->id, 'tipo' => 'button'])?>
+                    </div>
+                </td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
 </div>
