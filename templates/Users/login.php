ad447a3230f3b02d5b803af05e8d5960bde57bef
Arthu Santiago <contato@avds.eti.br>
2024-09-05 16:08:08 -0400

    feat: adicionado o autocomplete na tela de login

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -13,12 +13,12 @@
     </div>
     <div class="mb-3 elementoInterno">
       <label for="email" class="form-label text-light">Usuário</label>
-      <input type="text" class="form-control " id="email" name="email" placeholder="seu@e-mail.com" maxlength="100" required>
+      <input type="email" class="form-control " id="email" name="email" autocomplete="email" placeholder="seu@email.com" maxlength="100" required>
     </div>
     <div class="mb-3 elementoInterno">
       <label for="password" class="form-label text-light">Senha</label>
       <div class="input-group">
-        <input type="password" class="form-control" id="password" name="password" placeholder="******" minlength="12" required>
+        <input type="password" class="form-control" id="password" name="password" autocomplete="password" placeholder="******" minlength="12" required>
         <div class="btn-group">
           <button type="button" class="btn btn-secondary btn-revelar" data-revelar="password">
               <i class="bi bi-eye" aria-hidden="true" title="Revelar" data-revelar="password"></i>
@@ -28,7 +28,7 @@
     </div>
     <div class="mb-3 elementoInterno">
       <label for="2fa" class="form-label text-light" title="Two-Factor Authentication (2FA)">Segundo fator de autenticação</label>
-      <input type="number" class="form-control" id="2fa" name="2fa" minlength="6" title="Two-Factor Authentication (2FA)" required>
+      <input type="number" class="form-control" id="2fa" name="2fa" autocomplete="one-time-code" minlength="6" title="Two-Factor Authentication (2FA)" required>
     </div>
     <br>
     <div class="mb-3 text-end elementoInterno">
