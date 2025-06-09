b306dc2bb6c8d36044ad806274107dbbad5ee961
Arthu Santiago <contato@avds.eti.br>
2025-06-09 18:55:07 -0400

    feat: Melhorias e correcao de bugs (#53)
    * Modificando os nomes dos cookies para permitir a execucao no localhost no chrome
    
    * alterado a dinamica do minifcador de arquivos.
    
    * Criado helpers e melhorada toda a cadeia de utilizacao
    
    * utilizacao do novo padrao de verificao do ambiente de execucao
    
    * carregando os arquivos de css e js utilizando o novo padrão de verificao de ambiente
    
    * implementada nova maneira de atribuir valor na área de transferencia.
    
    * adicionado require dos paths de arquivos
    
    * adicionado o buscaConteudo.min.js
    
    * ajustado chamada de arquivos .js
    
    * alterada ordem de carregamento de arquivo .css
    
    * ajustado a busca de conteudo
    
    * Nova solucao do findUserPass

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -8,13 +8,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="sessionTimeout" id="sessionTimeout" content="<?=$sessionTimeout?>">
 	<?php
-	echo $this->Html->css([
-		'minified/bootstrap.min.css',
-		'minified/bootstrap-icons.min.css',
-		'minified/kaw.min.css'
-	]);
-	echo $this->fetch('css');
-	echo $this->fetch('script-first-load');
+		echo $this->Html->css('minified/bootstrap-icons.min.css', ['checarAmbiente' => false]);
+		echo $this->Html->css('minified/bootstrap.min.css', ['checarAmbiente' => false]);
+		echo $this->Html->css('geral.css');
+		echo $this->Html->css('responsivo.css');
+		echo $this->fetch('css');
+		echo $this->fetch('script-first-load');
 	?>
 </head>
 <body>
@@ -59,11 +58,11 @@
 	<?php
 		echo $this->element('buscaModal');
 		echo $this->element('Users/sessaoExpirada');
-		echo $this->Html->script('minified/popper.min.js');
-		echo $this->Html->script('minified/bootstrap.min.js');
-		echo $this->Html->script('minified/easytimer.min.js');
-		echo $this->Html->script('minified/ferramentas.min.js');
-		echo $this->Html->script('minified/buscaConteudo.min.js');
+		echo $this->Html->script('minified/popper.min.js', ['checarAmbiente' => false]);
+		echo $this->Html->script('minified/bootstrap.min.js', ['checarAmbiente' => false]);
+		echo $this->Html->script('minified/easytimer.min.js', ['checarAmbiente' => false]);
+		echo $this->Html->script('ferramentas.js');
+		echo $this->Html->script('buscaConteudo.js');
 		echo $this->fetch('script-last-load');
 	?>
 </body>
